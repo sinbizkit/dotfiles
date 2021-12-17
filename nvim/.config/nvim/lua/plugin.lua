462d7410d62c3795041497fb6467b56f1e060941
Ivan Berdnikov <sinbizkit@gmail.com>
2021-12-17 22:01:06 +0700

    Update i3-polybar configs. Add colorizer plugin for nvim.

--- nvim/.config/nvim/lua/plugin.lua
+++ nvim/.config/nvim/lua/plugin.lua
@@ -1,94 +1,99 @@
 vim.cmd [[ filetype plugin indent on ]]
 
 require 'packer'.startup({
-	function (use)
-		use('wbthomason/packer.nvim')
+  function (use)
+    use('wbthomason/packer.nvim')
 
-		use 'nvim-lua/popup.nvim'
+    use 'nvim-lua/popup.nvim'
 
-		use 'dracula/vim'
-		use {
-			'nvim-lualine/lualine.nvim',
-			requires = {'kyazdani42/nvim-web-devicons', opt = true}
-		}
+    use 'dracula/vim'
+    use {
+      'nvim-lualine/lualine.nvim',
+      requires = {'kyazdani42/nvim-web-devicons', opt = true}
+    }
 
-		use 'majutsushi/tagbar'
+    use 'majutsushi/tagbar'
 
-		use 'simrat39/symbols-outline.nvim'
-		use {
-			'folke/trouble.nvim',
-			requires = 'kyazdani42/nvim-web-devicons',
-		}
-		use 'tpope/vim-fugitive'
-		use {
-			'lewis6991/gitsigns.nvim',
-			requires = 'nvim-lua/plenary.nvim',
-			config = function() require 'gitsigns'.setup() end,
-		}
+    use 'simrat39/symbols-outline.nvim'
+    use {
+      'folke/trouble.nvim',
+      requires = 'kyazdani42/nvim-web-devicons',
+    }
+    use 'tpope/vim-fugitive'
+    use {
+      'lewis6991/gitsigns.nvim',
+      requires = 'nvim-lua/plenary.nvim',
+      config = function() require 'gitsigns'.setup() end,
+    }
 
-		use 'tpope/vim-surround'
-		use {
-			'phaazon/hop.nvim',
-			branch = 'v1', -- optional but strongly recommended
-			config = function() require'hop'.setup() end
-		}
-		use 'godlygeek/tabular'
+    use 'tpope/vim-surround'
+    use {
+      'phaazon/hop.nvim',
+      branch = 'v1', -- optional but strongly recommended
+      config = function() require'hop'.setup() end
+    }
+    use 'godlygeek/tabular'
 
-		use 'scrooloose/nerdcommenter'
-		use {
-			'kyazdani42/nvim-tree.lua',
-			requires = 'kyazdani42/nvim-web-devicons',
-		}
+    use 'scrooloose/nerdcommenter'
+    use {
+      'kyazdani42/nvim-tree.lua',
+      requires = 'kyazdani42/nvim-web-devicons',
+    }
 
-		use 'neovim/nvim-lspconfig'
-		use {
-			'hrsh7th/nvim-cmp',
-			requires = {
-				{ 'hrsh7th/cmp-nvim-lsp' },
-				{ 'hrsh7th/cmp-buffer' },
-				{ 'hrsh7th/cmp-path' },
-				{ 'onsails/lspkind-nvim' },
-			},
-		}
+    use 'neovim/nvim-lspconfig'
+    use {
+      'hrsh7th/nvim-cmp',
+      requires = {
+	{ 'hrsh7th/cmp-nvim-lsp' },
+	{ 'hrsh7th/cmp-buffer' },
+	{ 'hrsh7th/cmp-path' },
+	{ 'onsails/lspkind-nvim' },
+      },
+    }
 
-		use {
-			'ray-x/lsp_signature.nvim',
-			config = function() require 'lsp_signature'.setup() end
-		}
-		use 'dense-analysis/ale'
-		use 'SirVer/ultisnips'
+    use {
+      'ray-x/lsp_signature.nvim',
+      config = function() require 'lsp_signature'.setup() end
+    }
+    use 'dense-analysis/ale'
+    use 'SirVer/ultisnips'
 
-		use {
-			'nvim-treesitter/nvim-treesitter',
-			run = ':TSUpdate'
-		}
-		use {
-			'nvim-telescope/telescope.nvim',
-			requires = 'nvim-lua/plenary.nvim'
-		}
+    use {
+      'norcalli/nvim-colorizer.lua',
+      config = function() require 'colorizer'.setup() end
+    }
 
-		-- {{{ Language specific.
-		use {
-			'vim-scripts/a.vim',
-			ft = { 'c', 'cpp' },
-		}
-		use {
-			'rust-lang/rust.vim',
-			ft = 'rust' ,
-		}
-		use {
-			'simrat39/rust-tools.nvim',
-			ft = 'rust',
-			config = function() require 'rust-tools'.setup() end
-		}
-		-- }}}
+    use {
+      'nvim-treesitter/nvim-treesitter',
+      run = ':TSUpdate'
+    }
+    use {
+      'nvim-telescope/telescope.nvim',
+      requires = 'nvim-lua/plenary.nvim'
+    }
 
-	end,
-	-- display packer dialouge in the center in a floating window
-	config = {
-		display = {
-			open_fn = require('packer.util').float,
-		},
-	},
+    -- {{{ Language specific.
+    use {
+      'vim-scripts/a.vim',
+      ft = { 'c', 'cpp' },
+    }
+    use {
+      'rust-lang/rust.vim',
+      ft = 'rust' ,
+    }
+    use {
+      'simrat39/rust-tools.nvim',
+      ft = 'rust',
+      config = function() require 'rust-tools'.setup() end
+    }
+    -- }}}
+
+    end,
+    -- display packer dialouge in the center in a floating window
+    config = {
+    display = {
+      open_fn = require('packer.util').float,
+    },
+  },
 })
 
