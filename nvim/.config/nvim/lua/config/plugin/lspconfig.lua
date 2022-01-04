cba4f7b40b32b420bf106c868f665f771537cc84
Ivan Berdnikov <sinbizkit@gmail.com>
2022-01-04 18:11:40 +0700

    Refactor lspconfig's servers setup.

--- nvim/.config/nvim/lua/config/plugin/lspconfig.lua
+++ nvim/.config/nvim/lua/config/plugin/lspconfig.lua
@@ -3,15 +3,27 @@ local lspconfig = require 'lspconfig'
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
--- LSP Servers.
-lspconfig.clangd.setup{ capabilities = capabilities }
-lspconfig.gopls.setup{}
-lspconfig.cmake.setup{}
-lspconfig.pylsp.setup{}
-lspconfig.bashls.setup{}
-lspconfig.tsserver.setup{}
+local servers = { 'clangd', 'gopls', 'cmake', 'pylsp', 'bashls', 'tsserver' }
+local configs = {
+  clangd = { capabilities = capabilities },
+}
+
+-- Returns configuration for provided `server` if found, empty value otherwise.
+local configOrDefault = function(server)
+  local config = configs[server]
+  if config == nil then
+    return {}
+  end
+  return config
+end
 
 if (vim.fn.has('mac')) then
-  lspconfig.sourcekit.setup{ filetypes = { "swift", "objective-c", "objective-cpp" } }
+  table.insert(servers, 'sourcekit')
+  configs['sourcekit'] = { filetypes = { "swift", "objective-c", "objective-cpp" } }
+end
+
+for _, server in ipairs(servers) do
+  local config = configOrDefault(server)
+  lspconfig[server].setup(config)
 end
 
