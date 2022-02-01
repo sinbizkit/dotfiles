cedd1e628939c50e6dc000c647db0d22b1aa0677
Ivan Berdnikov <sinbizkit@gmail.com>
2022-02-01 12:29:13 +0700

    Fixed lspconfig setup.

--- nvim/.config/nvim/lua/config/plugin/lspconfig.lua
+++ nvim/.config/nvim/lua/config/plugin/lspconfig.lua
@@ -4,17 +4,16 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 local servers = { 'clangd', 'gopls', 'cmake', 'pylsp', 'bashls', 'tsserver' }
-local configs = {
-  clangd = { capabilities = capabilities },
-}
+local configs = {}
 
 -- Returns configuration for provided `server` if found, empty value otherwise.
+local defaultConfig = { capabilities = capabilities }
 local configOrDefault = function(server)
   local config = configs[server]
   if config == nil then
-    return {}
+    return defaultConfig
   end
-  return config
+  return vim.tbl_extend('force', defaultConfig, config)
 end
 
 if (vim.fn.has('mac')) then
