db132fa45adcf557de44ca908104eaf97e61ac43
Ivan Berdnikov <sinbizkit@gmail.com>
2022-04-27 22:40:39 +0700

    Update telescope conf.

--- nvim/.config/nvim/lua/sinbizkit/telescope.lua
+++ nvim/.config/nvim/lua/sinbizkit/telescope.lua
@@ -5,8 +5,15 @@ M.find_vimconf = function(opts)
 	opts = opts or {
 		prompt_title = 'NeoVim Config'
 	}
-	opts.cwd = '~/.config/nvim'
-	opts.search_dirs = { '~/.config/nvim' }
+
+	local path = vim.api.nvim_get_runtime_file("init.lua", false)
+	if #path ~= 1 then
+		error("Config directory not found.")
+	end
+	path = vim.fn.fnamemodify(path[1], ":p:h")
+
+	opts.cwd = path
+	opts.search_dirs = { path }
 	opts.file_ignore_patterns = {
 		'plugged/',
 	}
