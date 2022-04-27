baa4c210c8b92c0c28fbaf40431b126c1a36a59f
Ivan Berdnikov <sinbizkit@gmail.com>
2022-04-27 16:01:01 +0700

    Update Symbol-Outline config.

--- nvim/.config/nvim/lua/config/plugin/symbols-outline.lua
+++ nvim/.config/nvim/lua/config/plugin/symbols-outline.lua
@@ -1,4 +1,8 @@
 vim.g.symbols_outline = {
 	auto_preview = false,
-	position = 'left'
+	position = 'left',
+	relative_width = true,
+	width = 25,
+	show_relative_numbers = true,
+	show_symbol_details = false,
 }
