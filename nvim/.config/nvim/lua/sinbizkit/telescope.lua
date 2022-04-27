a100a6da2bf5d25f189d44bb48cfc978ab1f2ec7
Ivan Berdnikov <sinbizkit@gmail.com>
2022-04-27 15:42:49 +0700

    Update Telescope settings.

--- nvim/.config/nvim/lua/sinbizkit/telescope.lua
+++ nvim/.config/nvim/lua/sinbizkit/telescope.lua
@@ -2,11 +2,14 @@ local M = {}
 
 -- Search vim config files.
 M.find_vimconf = function(opts)
-	local opts = opts or {
+	opts = opts or {
 		prompt_title = 'NeoVim Config'
 	}
 	opts.cwd = '~/.config/nvim'
 	opts.search_dirs = { '~/.config/nvim' }
+	opts.file_ignore_patterns = {
+		'plugged/',
+	}
 	require('telescope.builtin').find_files(opts)
 end
 
