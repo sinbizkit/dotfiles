131bbe183d62d29ba7b9a0a45af60709802a75c8
Ivan Berdnikov <sinbizkit@gmail.com>
2022-09-30 02:23:03 +0000

    Update lualine config.

--- nvim/.config/nvim/lua/config/plugin/lualine.lua
+++ nvim/.config/nvim/lua/config/plugin/lualine.lua
@@ -1,7 +1,7 @@
 require("lualine").setup {
   options = {
-    section_separators = { left = "", right = "" },
-    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    component_separators = { left = "", right = "" },
   },
   sections = {
     lualine_a = { "mode" },
