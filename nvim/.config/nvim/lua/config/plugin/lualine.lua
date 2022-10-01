6634b4e83b25a0a0629a9b1e4b7c80d54fdde9f8
Ivan Berdnikov <sinbizkit@gmail.com>
2022-10-01 14:32:10 +0700

    Update lualine config.

--- nvim/.config/nvim/lua/config/plugin/lualine.lua
+++ nvim/.config/nvim/lua/config/plugin/lualine.lua
@@ -3,15 +3,26 @@ require("lualine").setup {
     section_separators = { left = "", right = "" },
     component_separators = { left = "", right = "" },
   },
+
   sections = {
     lualine_a = { "mode" },
     lualine_b = { "branch" },
-    lualine_c = { "filename" },
+    lualine_c = { { "filename" }, { "diff" } },
     lualine_x = { "encoding", "fileformat", "filetype" },
     lualine_y = {
       { "diagnostics", sources = { "nvim_diagnostic" } },
-      "progress",
+      { "progress" },
     },
     lualine_z = { "location" },
+  },
+
+  tabline = {
+    lualine_a = {
+      {
+        "tabs",
+        max_length = vim.o.columns,
+        mode = 2,
+      },
+    },
   },
 }
