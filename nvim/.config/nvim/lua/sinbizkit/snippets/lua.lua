1609065fe8c5de49828f2d53a910de8be843774f
Ivan Berdnikov <sinbizkit@gmail.com>
2022-12-20 15:15:47 +0000

    Added .lua snippet for pcall.

--- nvim/.config/nvim/lua/sinbizkit/snippets/lua.lua
+++ nvim/.config/nvim/lua/sinbizkit/snippets/lua.lua
@@ -4,6 +4,7 @@ local fmt = require("luasnip.extras.fmt").fmt
 local s = ls.s
 local i = ls.i
 local f = ls.f
+local rep = require("luasnip.extras").rep
 
 return {
   s(
@@ -13,6 +14,17 @@ return {
         local splitted = vim.split(args[1][1], ".", { plain = true })
         return splitted[#splitted]:gsub("-", "_")
       end, { 1 }),
+      i(1),
+    })
+  ),
+  s(
+    "pc",
+    fmt([[local has_{}, {} = pcall(require, "{}")]], {
+      f(function(args)
+        local splitted = vim.split(args[1][1], ".", { plain = true })
+        return splitted[#splitted]:gsub("-", "_")
+      end, { 1 }),
+      rep(1),
       i(1),
     })
   ),
