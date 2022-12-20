7e7cfdc5f7514857e3d42c9261b3186f8892a656
Ivan Berdnikov <sinbizkit@gmail.com>
2022-12-20 15:39:27 +0000

    Fix pc-snippet for lua.

--- nvim/.config/nvim/lua/sinbizkit/snippets/lua.lua
+++ nvim/.config/nvim/lua/sinbizkit/snippets/lua.lua
@@ -4,27 +4,25 @@ local fmt = require("luasnip.extras.fmt").fmt
 local s = ls.s
 local i = ls.i
 local f = ls.f
-local rep = require("luasnip.extras").rep
+
+local mod_name = function(args)
+  local splitted = vim.split(args[1][1], ".", { plain = true })
+  return splitted[#splitted]:gsub("-", "_")
+end
 
 return {
   s(
     "req",
     fmt([[local {} = require "{}"]], {
-      f(function(args)
-        local splitted = vim.split(args[1][1], ".", { plain = true })
-        return splitted[#splitted]:gsub("-", "_")
-      end, { 1 }),
+      f(mod_name, { 1 }),
       i(1),
     })
   ),
   s(
     "pc",
     fmt([[local has_{}, {} = pcall(require, "{}")]], {
-      f(function(args)
-        local splitted = vim.split(args[1][1], ".", { plain = true })
-        return splitted[#splitted]:gsub("-", "_")
-      end, { 1 }),
-      rep(1),
+      f(mod_name, { 1 }),
+      f(mod_name, { 1 }),
       i(1),
     })
   ),
