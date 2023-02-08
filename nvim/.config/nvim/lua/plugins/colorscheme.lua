e2758e67c86db98a00388bcce4affd63aed6eac6
Ivan Berdnikov <sinbizkit@gmail.com>
2023-02-08 06:17:59 +0000

    Update tokyonight highlight config.

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,13 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
+    opts = {
+      on_highlights = function(h, _)
+        h["@type.qualifier"] = { link = "@keyword" }
+      end,
+    },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
       vim.cmd [[colorscheme tokyonight]]
     end,
   },
