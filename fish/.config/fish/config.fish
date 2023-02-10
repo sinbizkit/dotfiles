cb6c864a574e2416afd0bb441deca34380202a81
Ivan Berdnikov <sinbizkit@gmail.com>
2023-02-10 17:58:04 +0700

    Restore fish ctrl-f mapping from emacs mode.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,10 @@
 
 # vi-keybindings by default
 fish_vi_key_bindings
+# Restore "<Ctrl>f" mapping from emacs mode.
+for mode in insert default visual
+	bind -M $mode \cf forward-char
+end
 
 # Supress greetings message.
 function fish_greeting
