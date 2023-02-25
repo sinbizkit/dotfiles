8e8cbdbabe2b5c39d3c350bcb0b01946d9c27f9e
Ivan Berdnikov <sinbizkit@gmail.com>
2023-02-25 23:34:41 +0700

    Add fish nvim alias.

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -1,6 +1,6 @@
 # Set vim as alias for neovim for macOS. Use update-alternatives for
 # nix platforms.
-if test (uname) = "Darwin"; and test -e (which nvim)
+if type -q nvim
 	alias vim="nvim"
 end
 
