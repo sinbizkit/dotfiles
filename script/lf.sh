6b8f29409aa0f249aa939ef4cf86fba848b3f36c
Victor <victor@unigine.com>
2023-03-02 22:57:56 +0700

    Installation 'lf' and 'tmux' for linux-s with 'apt` as a package manager.

--- script/lf.sh
+++ script/lf.sh
@@ -4,3 +4,8 @@ if [[ -f "/etc/arch-release" ]]; then
 	sudo pacman -S --needed lf
 	exit $?
 fi
+
+shell=/bin/bash
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+$shell "$script_dir/install_from_github_archive.sh" gokcehan lf lf-linux-amd64.tar.gz
+exit $?
--- script/tmux.sh
+++ script/tmux.sh
@@ -4,3 +4,8 @@ if [[ -f "/etc/arch-release" ]]; then
 	sudo pacman -S --needed tmux
 	exit $?
 fi
+
+shell=/bin/bash
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+$shell "$script_dir/install_by_package_manager.sh" tmux
+exit $?
