5fc41d10d6bfdeac1c740a7bfaa99db56690d111
Ivan Berdnikov <sinbizkit@gmail.com>
2024-04-23 09:55:19 +0700

    feat(nvim): add TreeSitter `json` module as ensured.

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -34,6 +34,7 @@ return {
       "cmake",
       "bash",
       "javascript",
+      "json",
     },
     highlight = { -- enable highlighting for all file types
       enable = true, -- you can also use a table with list of langs here (e.g. { "python", "javascript" })
