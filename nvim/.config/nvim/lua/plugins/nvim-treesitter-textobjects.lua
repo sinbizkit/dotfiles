2eccf04f8b823784bfa2cb3e5ec7c0e473949e8a
Ivan Berdnikov <ivan.berdnikov@kaspersky.com>
2024-11-27 13:19:41 +0300

    fix(nvim): remove conficting treesitter objects (]c,[c and etc)

--- nvim/.config/nvim/lua/plugins/nvim-treesitter-textobjects.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter-textobjects.lua
@@ -26,19 +26,15 @@ return {
         set_jumps = true, -- whether to set jumps in the jumplist.
         goto_next_start = {
           ["]f"] = "@function.outer",
-          ["]c"] = "@class.outer",
         },
         goto_next_end = {
           ["]F"] = "@function.outer",
-          ["]C"] = "@class.outer",
         },
         goto_previous_start = {
           ["[f"] = "@function.outer",
-          ["[c"] = "@class.outer",
         },
         goto_previous_end = {
           ["[F"] = "@function.outer",
-          ["[C"] = "@class.outer",
         },
       },
     },
