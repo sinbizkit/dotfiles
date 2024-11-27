136314b9f1a9890d1c509e72ae36f4949b98a351
Ivan Berdnikov <ivan.berdnikov@kaspersky.com>
2024-11-27 13:09:57 +0300

    feat(nvim): add fugitive keymaps.

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -4,7 +4,9 @@ local M = {
 
 function M.config()
   local km = require "sinbizkit.keymap"
+  km.map("n", "<Leader>G", ":Git<CR>") -- fugitive main window
   km.map("n", "<Leader>ha", ":Git blame<CR>") -- git commit author
+  km.map("v", "<Leader>ha", ":'<,'>Git blame<CR>") -- git commit author
 end
 
 return M
