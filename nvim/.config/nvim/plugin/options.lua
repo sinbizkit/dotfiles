33a08b9bd4dd023f96d949fc93481aef25d7ce22
Ivan Berdnikov <ivan.berdnikov@kaspersky.com>
2025-04-01 09:45:38 +0300

    chore(nvim): update diagnostic config.

--- nvim/.config/nvim/plugin/options.lua
+++ nvim/.config/nvim/plugin/options.lua
@@ -79,9 +79,9 @@ vim.diagnostic.config {
     },
     linehl = {
       [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
-      [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
-      [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
-      [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
     },
   },
 }
