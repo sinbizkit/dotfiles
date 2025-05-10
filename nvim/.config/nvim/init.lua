ef208dbe0c605067ade416c9a94ddf804e381926
Ivan Berdnikov <sinbizkit@gmail.com>
2025-05-10 22:36:49 +0700

    fix(nvim): update init configuration.
    * Use vim.uv.fs_stat instead of deprecated vim.loop.
    * use vim.system instead of vim.fn.system.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,15 +3,17 @@ vim.cmd [[ filetype plugin indent on ]]
 
 -- init lazy.nvim
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  }
+if not vim.uv.fs_stat(lazypath) then
+  vim
+    .system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazypath,
+    })
+    :wait()
 end
 
 -- Add lazy to the `runtimepath`, this allows us `require` it.
