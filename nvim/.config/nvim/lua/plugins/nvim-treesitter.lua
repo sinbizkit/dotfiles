c3f201c44742f5c5bdcc634e271385d748c8460d
Ivan Berdnikov <sinbizkit@gmail.com>
2025-05-14 12:10:22 +0700

    chore(nvim): update nvim-treesitter cfg.

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,23 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   event = { "BufReadPre", "BufNewFile" },
-  cmd = {
-    "TSInstall",
-    "TSInstallSync",
-    "TSInstallInfo",
-    "TSUpdate",
-    "TSUpdateSync",
-    "TSUninstall",
-    "TSBufEnable",
-    "TSBufDisable",
-    "TSBufToggle",
-    "TSEnable",
-    "TSDisable",
-    "TSToggle",
-    "TSModuleInfo",
-    "TSEditQuery",
-    "TSEditQueryUserAfter",
-  },
   build = ":TSUpdate",
   dependencies = {
     "nvim-treesitter/nvim-treesitter-context",
@@ -27,6 +10,11 @@ return {
     ensure_installed = {
       "c",
       "cpp",
+      "query",
+      "vim",
+      "vimdoc",
+      "markdown",
+      "markdown_inline",
       "rust",
       "go",
       "lua",
@@ -36,6 +24,7 @@ return {
       "javascript",
       "json",
     },
+    sync_install = false,
     highlight = { -- enable highlighting for all file types
       enable = true, -- you can also use a table with list of langs here (e.g. { "python", "javascript" })
     },
