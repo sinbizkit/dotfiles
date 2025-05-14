47cb640f549b2055987d62fa00f57dbb7c09dc08
Ivan Berdnikov <sinbizkit@gmail.com>
2025-05-14 12:16:57 +0700

    chore(nvim): update nvim-treesitter cfg.

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -27,6 +27,14 @@ return {
     sync_install = false,
     highlight = { -- enable highlighting for all file types
       enable = true, -- you can also use a table with list of langs here (e.g. { "python", "javascript" })
+      disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+        return false
+      end,
     },
     indent = { -- indentation based on treesitter for the |=| operator.
       enable = false,
