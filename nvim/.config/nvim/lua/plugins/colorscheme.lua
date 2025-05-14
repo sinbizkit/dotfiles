2d247bb7119ccb48190d11aa9f3e41adbadf4813
Ivan Berdnikov <ivan.berdnikov@kaspersky.com>
2025-05-14 10:39:04 +0300

    chore(nvim): update colorscheme plugin cfg.

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,39 +1,38 @@
-return {
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function(_, opts)
-      -- Default options:
-      require('kanagawa').setup({
-        compile = false, -- enable compiling the colorscheme
-        undercurl = true, -- enable undercurls
-        commentStyle = { italic = true },
-        functionStyle = {},
-        keywordStyle = { italic = true },
-        statementStyle = { bold = true },
-        typeStyle = {},
-        transparent = false, -- do not set background color
-        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-        terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        colors = {       -- add/modify theme and palette colors
-          palette = {
-            dragonBlack6 = "#44413e"
-          },
-          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-        },
-        overrides = function(colors) -- add/modify highlights
-          return {
-            ["@lsp.type.comment"] = { link = "@comment" }
-          }
-        end,
-        theme = "wave", -- Load "wave" theme when 'background' option is not set
-        background = { -- map the value of 'background' option to a theme
-          dark = "wave", -- try "dragon" !
-          light = "lotus"
-        },
-      })
-      vim.cmd [[colorscheme kanagawa-dragon]]
-    end,
-  },
+local M = {
+  "rebelot/kanagawa.nvim",
+  lazy = false,
+  priority = 1000,
 }
+
+-- Default options:
+M.opts = {
+  compile = false, -- enable compiling the colorscheme
+  undercurl = true, -- enable undercurls
+  commentStyle = { italic = true },
+  functionStyle = {},
+  keywordStyle = { italic = true },
+  statementStyle = { bold = true },
+  typeStyle = {},
+  transparent = false, -- do not set background color
+  dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+  terminalColors = true, -- define vim.g.terminal_color_{0,17}
+  colors = { -- add/modify theme and palette colors
+    palette = {
+      dragonBlack6 = "#44413e",
+    },
+    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+  },
+  overrides = function(colors) -- add/modify highlights
+    return {
+      ["@lsp.type.comment"] = { link = "@comment" },
+    }
+  end,
+}
+
+M.config = function(_, opts)
+  local kanagawa = require "kanagawa"
+  kanagawa.setup(opts)
+  kanagawa.load "dragon"
+end
+
+return M
