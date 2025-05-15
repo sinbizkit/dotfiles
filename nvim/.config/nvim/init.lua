3311ff6e97f236c9a844406c28b907ef53d3d4ff
Ivan Berdnikov <ivan.berdnikov@kaspersky.com>
2025-05-15 14:40:47 +0300

    feat(nvim): update lazy.nvim initialization.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,17 +3,25 @@ vim.cmd [[ filetype plugin indent on ]]
 
 -- init lazy.nvim
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-  vim
-    .system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable", -- latest stable release
-      lazypath,
-    })
-    :wait()
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    lazyrepo,
+    lazypath,
+  }
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 
 -- Add lazy to the `runtimepath`, this allows us `require` it.
