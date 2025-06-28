ec2372d825700a3f8e4dc8c4a06f6441eee5090c
Ivan Berdnikov <sinbizkit@gmail.com>
2025-06-28 13:05:48 +0300

    refactor(nvim): update lsp_signature configuration.

--- nvim/.config/nvim/lua/plugins/lsp_signature.lua
+++ nvim/.config/nvim/lua/plugins/lsp_signature.lua
@@ -1,16 +1,11 @@
 local M = {
   "ray-x/lsp_signature.nvim",
+  event = "InsertEnter",
+  opts = {
+    bind = true,
+    floating_window = true,
+    hint_enable = false,
+  },
 }
-
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("sb-lsp_signature", {}),
-  callback = function (args)
-    if not (args.data and args.data.client_id) then
-      return
-    end
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    require"lsp_signature".on_attach(client, args.buf)
-  end,
-})
 
 return M
