f0efba445b36bca1839513110bb33f4c6ef6d0ff
Ivan Berdnikov <sinbizkit@gmail.com>
2025-09-07 15:51:48 +0300

    revert(nvim): revert unwanted changed in lsp config

--- nvim/.config/nvim/plugin/lsp.lua
+++ nvim/.config/nvim/plugin/lsp.lua
@@ -52,9 +52,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
       return
     end
     map_lsp_keys() -- map lsp-specific keymaps.
-    -- local client = vim.lsp.get_client_by_id(args.data.client_id)
-    -- if client and client.server_capabilities.inlayHintProvider then
-    --   vim.lsp.inlay_hint.enable(true)
-    -- end
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and client.server_capabilities.inlayHintProvider then
+      vim.lsp.inlay_hint.enable(true)
+    end
   end,
 })
