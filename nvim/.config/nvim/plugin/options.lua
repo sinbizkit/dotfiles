c866556a301f078b8d2a3cb8dfee21e73054e1e2
Ivan Berdnikov <sinbizkit@gmail.com>
2026-05-03 19:41:00 +0700

    chore(nvim): update config due to v0.12.

--- nvim/.config/nvim/plugin/options.lua
+++ nvim/.config/nvim/plugin/options.lua
@@ -68,6 +68,7 @@ opt.completeopt = { "noinsert", "menuone", "noselect" }
 opt.laststatus = 2 -- The last window will always have a status line.
 
 vim.diagnostic.config {
+  severity_sort = true,
   virtual_text = true,
   virtual_lines = false,
   signs = {
