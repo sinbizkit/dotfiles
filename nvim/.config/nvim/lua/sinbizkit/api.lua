aaf59b4a1ec191211a272c1d418e0ba4373e0bbf
Ivan Berdnikov <sinbizkit@gmail.com>
2026-08-25 15:59:41 +0700

    fix(nvim): push missing api.lua

---
+++ nvim/.config/nvim/lua/sinbizkit/api.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+function M.get_cur_buf_filename()
+  local filepath = vim.api.nvim_buf_get_name(0)
+  if filepath == "" then
+    return filepath
+  end
+  local filename = string.match(filepath, "/[%w.]+$")
+  if filename == nil then
+    return ""
+  end
+  return string.sub(filename, 2, #filename)
+end
+
+return M
